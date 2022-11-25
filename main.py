bb26b33a363c222dde8d164c9148ff2df2ba4f7a
takumi <y.takumi4@gmail.com>
2022-11-26 02:02:44 +0900

    success_messageを修正

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def handler(request):
             logger.error(e)
             sys.exit()
         
-        success_message = '連携完了しました'
+        success_message = success_message = 'LINE連携いただきありがとうございます! \n正常に連携完了しました。\n\nLINE連携により以下のことができるようになります。\n・シフト通知機能\n・シフト締め切り通知機能\n\nなお、LINE連携を解除したい場合は、ブロックしていただくと連携を解除することができます。'
         LINE_BOT_API.push_message(line_user_id, TextSendMessage(text=success_message))
         logger.info('連携完了メッセージの送信が完了しました')
     LINE_HANDLER.handle(body, signature)
